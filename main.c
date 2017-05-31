0fa1344b3d8b6bda7917c04a17bea85b68ede67d
Kevin Hernández Rostrán <kevinah95@gmail.com>
2017-05-31 09:20:57 -0600

    Json object

--- main.c
+++ main.c
@@ -1,7 +1,52 @@
-#include<stdio.h>
+#include "parson.h"
 
-main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void serialization_example(void) {
+    char *serialized_string = NULL;
+
+    JSON_Value *root_value = json_value_init_array();
+    JSON_Array *root_object = json_value_get_array(root_value);
+
+
+    JSON_Value *schema = json_value_init_object();
+    JSON_Object *schema_obj = json_value_get_object(schema);
+    json_object_set_number(schema_obj, "tamanio", 10);
+    json_object_set_number(schema_obj, "disponible", 3);
+    //------------Procesos
+    JSON_Value *procesos_value = json_value_init_array();
+    JSON_Array *procesos_object = json_value_get_array(procesos_value);
+
+    JSON_Value *schema_process = json_value_init_object();
+    JSON_Object *schema_process_obj = json_value_get_object(schema_process);
+    json_object_set_string(schema_process_obj,"nombre","P1");
+    json_object_set_number(schema_process_obj,"utiliza",5);
+    json_object_set_number(schema_process_obj,"duracion",30);
+    json_array_append_value(procesos_object,schema_process);
+    //-----
+    json_object_set_value(schema_obj,"procesos",procesos_value);
+
+    
+    json_array_append_value(root_object,schema);
+    
+    serialized_string = json_serialize_to_string_pretty(root_value);
+    puts(serialized_string);
+    JSON_Value *test_value = json_parse_string("[{\"tamanio\":10,\"disponible\":3,\"procesos\":[{\"name\":\"P1\",\"utiliza\":5,\"tiempo\":30}]},{\"tamanio\":5,\"disponible\":2,\"procesos\":[{\"name\":\"P2\",\"utiliza\":3,\"tiempo\":37}]}]");
+    JSON_Array *test_arr = json_value_get_array(test_value);
+    //JSON_Array *test_arr = json_object_get_array(test_obj);
+    char *serialized_string_01 = NULL;
+    serialized_string_01 = json_serialize_to_string_pretty(test_value);
+    puts(serialized_string_01);
+    size_t count = json_array_get_count(test_arr);
+    printf("%d",count);
+    json_free_serialized_string(serialized_string);
+    json_value_free(root_value);
+}
+
+int main()
 {
-    printf("Hello World!");
-
+    serialization_example();
+    return 0;
 }
---
+++ mapwritejson.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+#include "parson.h"
+
+int main(int argc, const char *argv[])
+{
+        
+    /* Open a file for writing.
+     *  - Creating the file if it doesn't exist.
+     *  - Truncating it to 0 size if it already exists. (not really needed)
+     *
+     * Note: "O_WRONLY" mode is not sufficient when mmaping.
+     */
+    
+    const char *filepath = "/tmp/mmapped.bin";
+
+    int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+    
+    if (fd == -1)
+    {
+        perror("Error opening file for writing");
+        exit(EXIT_FAILURE);
+    }
+
+    // Stretch the file size to the size of the (mmapped) array of char
+    JSON_Value *root_value = json_value_init_object();
+    JSON_Object *root_object = json_value_get_object(root_value);
+    char *serialized_string = NULL;
+    json_object_set_string(root_object, "name", "John Smith");
+    serialized_string = json_serialize_to_string_pretty(root_value);
+
+    size_t textsize = strlen(serialized_string) + 1; // + \0 null character
+    
+    if (lseek(fd, textsize-1, SEEK_SET) == -1)
+    {
+        close(fd);
+        perror("Error calling lseek() to 'stretch' the file");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Something needs to be written at the end of the file to
+     * have the file actually have the new size.
+     * Just writing an empty string at the current file position will do.
+     *
+     * Note:
+     *  - The current position in the file is at the end of the stretched 
+     *    file due to the call to lseek().
+     *  - An empty string is actually a single '\0' character, so a zero-byte
+     *    will be written at the last byte of the file.
+     */
+    
+    if (write(fd, "", 1) == -1)
+    {
+        close(fd);
+        perror("Error writing last byte of the file");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    // Now the file is ready to be mmapped.
+    char *map = mmap(0, textsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (map == MAP_FAILED)
+    {
+        close(fd);
+        perror("Error mmapping the file");
+        exit(EXIT_FAILURE);
+    }
+    
+    strcpy (map, serialized_string);
+    puts(map);
+
+    // Write it now to disk
+    if (msync(map, textsize, MS_SYNC) == -1)
+    {
+        perror("Could not sync the file to disk");
+    }
+    
+    // Don't forget to free the mmapped memory
+    if (munmap(map, textsize) == -1)
+    {
+        close(fd);
+        json_free_serialized_string(serialized_string);
+        json_value_free(root_value);
+        perror("Error un-mmapping the file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Un-mmaping doesn't close the file, so we still need to do that.
+    close(fd);
+
+    json_free_serialized_string(serialized_string);
+    json_value_free(root_value);
+    
+    return 0;
+}
